fb8a26c2a94b7be9a1c8b231ab2f8239b809c7bc
Hamza <muhammad.hamza@experia.ai>
2022-09-21 16:20:44 +0500

    saga file creation and generator functions added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,18 @@
 // import {createStore} from 'redux';
-import {configureStore} from '@reduxjs/toolkit';
-import rootReducer from './rootReducer'
+import { configureStore } from '@reduxjs/toolkit';
+import rootReducer from './rootReducer';
+import productSaga from './productSaga';
+import createSagaMddleware from 'redux-saga';
 
 // const store = createStore(rootReducer)
-const store = configureStore({reducer:rootReducer})
+const sagaMiddleware = createSagaMddleware();
+const store = configureStore(
+    {
+        reducer: rootReducer,
+        middleware: () => [sagaMiddleware]
+    }
+)
+
+sagaMiddleware.run(productSaga);
 
 export default store;
